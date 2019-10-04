3d853efaec03aea977eb2c7655cf154e452f9ad9
Maikeaerosmith <maicon.friedel@gmail.com>
2019-10-04 20:54:22 -0300

    Added Socket.io

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Link } from "react-router-dom";
+import socketio from "socket.io-client";
 
 import api from "../../services/api";
 import "./styles.css";
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
+  const user_id = localStorage.getItem("user");
+  const socket = useMemo(
+    () =>
+      socketio("http://10.0.0.6:3333", {
+        query: { user_id }
+      }),
+    [user_id]
+  );
+
+  useEffect(() => {
+    socket.on("booking_request", data => setRequests([...requests, data]));
+  }, [requests, socket]);
 
   useEffect(() => {
     async function loadSpots() {
@@ -20,8 +35,38 @@ export default function Dashboard() {
     loadSpots();
   }, []);
 
+  async function handleAccept(id) {
+    await api.post(`/bookings/${id}/approvals`);
+
+    setRequests(requests.filter(req => req._id !== id));
+  }
+
+  async function handleReject(id) {
+    await api.post(`/bookings/${id}/rejections`);
+
+    setRequests(requests.filter(req => req._id !== id));
+  }
+
   return (
     <>
+      <ul className="notifications">
+        {requests.map(req => (
+          <li key={req._id}>
+            <p>
+              <strong>{req.user.email}</strong> está solicitando uma reserva em{" "}
+              <strong>{req.spot.company}</strong> para a data:{" "}
+              <strong>{req.date}</strong>
+            </p>
+            <button onClick={() => handleAccept(req._id)} className="accept">
+              ACEITAR
+            </button>
+            <button onClick={() => handleReject(req._id)} className="reject">
+              REJEITAR
+            </button>
+          </li>
+        ))}
+      </ul>
+
       <ul className="spot-list">
         {spots &&
           spots.map(spot => (
